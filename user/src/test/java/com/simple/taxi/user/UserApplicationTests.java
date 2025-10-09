2403f2eceab8cd7961fac6c5e4dcf8cdda24a7c9
Yahimchik <egor.yaximovich@gmail.com>
2025-10-09 21:20:10 +0300

    added user service package

---
+++ user/src/test/java/com/simple/taxi/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.taxi.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
